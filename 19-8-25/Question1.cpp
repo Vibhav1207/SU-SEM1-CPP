f39b13ee0700c2feacdac4d95014a972d4e5b9d9
Vibhav Patel <77201529+Vibhav1207@users.noreply.github.com>
2025-08-19 11:47:55 +0530

    Added todays filesz

---
+++ 19-8-25/Question1.cpp
@@ -0,0 +1,21 @@
+//WAP pt check the the number is odd or even
+
+#include <iostream>
+using namespace std;
+ int checkOddEven(int num) {
+    if(num%2 ==0){
+        return 0;
+    }
+    else{
+        return 1; 
+    }
+ }
+ int main () {
+    int number;
+    cout << "Enter a number: ";
+    cin>>number;
+    if(checkOddEven(number) == 0)
+        cout << number << " is even." << endl;
+    else
+        cout << number << " is odd." << endl;
+ }
